1386e2eae067a4508e61df868a58851e19b218dc
ardithlutf <ardith.lutfiawan@gmail.com>
2022-02-12 14:22:19 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/warung/ihwani/warung_ihwani/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.warung.ihwani.warung_ihwani
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:warung_ihwani/src/ui/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(0,67,67, 1),
+        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
